859c3cd71d268cc7bb7f53605315538b2a924a11
Jann Nathaniel Jaravata <jannjaravata@gmail.com>
2026-06-10 16:53:58 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const modalTitle = document.querySelector("#modalTitle");
 const modalDescription = document.querySelector("#modalDescription");
 const modalLink = document.querySelector("#modalLink");
 
-const SITE_VERSION = "1.1.4";
+const SITE_VERSION = "1.1.5";
 
 document.querySelector("#year").textContent = new Date().getFullYear();
 
